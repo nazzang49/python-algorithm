2895f80de1d3869f491c77c179bcf15e93810c88
박진영 <nazzang49@naver.com>
2021-09-15 01:38:12 +0900

    add files

---
+++ greedy/week2/b_14711.py
@@ -0,0 +1,20 @@
+def get_max_score():
+    tmp_list = [0 for _ in range(1000)]
+
+    for i in range(n):
+        # 현재 마감일 기준 => 더 짧은 마감일 중 갱신된적 없다면
+        for j in range(scores[i][0] - 1, -1, -1):
+            if tmp_list[j] == 0:
+                tmp_list[j] = scores[i][1]
+                break
+    return sum(tmp_list)
+
+n = int(input())
+scores = []
+
+for _ in range(n):
+    d, s = map(int, input().split())
+    scores.append((d, s))
+
+scores = sorted(scores, key=lambda x: x[1], reverse=True)
+print(get_max_score())
